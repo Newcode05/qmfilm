b9417a33312eb9f319402854f13073805f86b796
newcode05 <manhpham771977@gmail.com>
2025-09-21 23:52:08 +0700

    feat(FABMenu): add FABMenu component

---
+++ app/components/FAB menu/FABmenu.style.tsx
@@ -0,0 +1,17 @@
+import { tv } from "tailwind-variants";
+
+export const FABMenuStyle = tv({
+  base: "",
+  variants: {
+    role: {
+      container: "",
+      listitem: "",
+      fab: "",
+    },
+    color: {
+      primary: "",
+      secondary: "",
+      tertiary: "",
+    },
+  },
+});
---
+++ app/components/FAB menu/FABmenu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const FABMenu = () => {
+  return (
+    <motion.div>
+      <div></div>
+      <div></div>
+    </motion.div>
+  );
+};
